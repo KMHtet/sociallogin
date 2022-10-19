bc3cc10b85f40604fc9a389f7f4b2273c3f714dd
Kyaw Myo Htet <kyawmyohtet@mytel.com.mm>
2022-10-19 15:16:59 +0630

    add foreground noti with notifee

--- src/utils/PushNotificationUtils.tsx
+++ src/utils/PushNotificationUtils.tsx
@@ -41,44 +41,16 @@ export const notificationListener = () => {
 
     // Foreground state messages
     messaging().onMessage(async remoteMessage => {
+
         console.warn('kmh', 'Message handled in the foreground!', remoteMessage);
 
-        if(Platform.OS === 'android'){
-            await notifee.createChannel({
-                id: 'default',
-                name: 'Default',
-                lights: false,
-                vibration: true,
-                importance: AndroidImportance.HIGH,
-                badge: true,
-                visibility: AndroidVisibility.PRIVATE,
-              });
-        }
         await notifee.displayNotification({
             title: remoteMessage.notification?.title,
             body: remoteMessage.notification?.body,
             android: {
               channelId: 'default',
-              visibility: AndroidVisibility.PRIVATE,
-              largeIcon: remoteMessage.notification?.android?.imageUrl,
-              style: remoteMessage.notification?.android?.imageUrl
-                ? {
-                    type: AndroidStyle.BIGPICTURE,
-                    picture: remoteMessage.notification?.android?.imageUrl,
-                    largeIcon: null,
-                  }
-                : undefined,
             },
-            ios: {
-              categoryId: 'default',
-              foregroundPresentationOptions: {
-                alert: true,
-                badge: true,
-                sound: true,
-              },
-              critical: true,
-            },
-            data: remoteMessage.data,
+            data: remoteMessage?.data,
           });
     })
 }
